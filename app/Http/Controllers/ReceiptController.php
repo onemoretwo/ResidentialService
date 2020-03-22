05c8ee42546b91571942a5664fe630ef0cb17faa
Arnon Suksiri <arnon.suksili@gmail.com>
2020-03-22 23:04:38 +0700

    create bill

--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Bill;
 use App\Building;
 use App\Report;
 use App\Room;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ReceiptController extends Controller
 {
@@ -37,7 +39,7 @@ class ReceiptController extends Controller
         $building_floor = $request->input('building_floor');
         $room_number = $request->input('room_number');
         $room_id = Room::where('building_id',$building_id)->where('floor',$building_floor)->where('number',$room_number)->first()->id;
-        $reports = Report::where('room_id',$room_id)->where('type','รายงาน')->where('status','รอการยืนยัน')->get();
+        $reports = Report::where('room_id',$room_id)->where('type','รายงาน')->where('status','รอการยืนยัน')->orderBy('created_at','desc')->get();
         return view('receipts.create',['reports' => $reports]);
     }
 
@@ -49,7 +51,31 @@ class ReceiptController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $building_name = $request->input('building_name');
+        $building_id = Building::where('name',$building_name)->first()->id;
+        $building_floor = $request->input('building_floor');
+        $room_number = $request->input('room_number');
+        $room = Room::where('building_id',$building_id)->where('floor',$building_floor)->where('number',$room_number)->first();
+        $price = $request->input('price');
+        $water_unit = $request->input('w_unit');
+        $electric_unit = $request->input('e_unit');
+        $w_rate = $room->building->water_rate;
+        $e_rate = $room->building->electric_rate;
+        $totalPrice = $price + ($w_rate * $water_unit) + ($e_rate * $electric_unit);
+
+        $bill = new Bill();
+        $bill->room_id = $room->id;
+        $bill->user_id = Auth::id();
+        $bill->water_unit = $water_unit;
+        $bill->electric_unit = $electric_unit;
+        $bill->room_price = $price;
+        $bill->total_price = $totalPrice;
+        $bill->save();
+
+
+
+
+
     }
 
     /**
