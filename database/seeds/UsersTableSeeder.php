3829a2a8bef69155e5e5ccc15618dcd8f0f1c45c
Kantarat <Kuntarut.k@ku.th>
2020-04-19 21:17:16 +0700

    update seeder and readme

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,22 +13,22 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-//        $user = new User();
-//        $user->room_id = null;
-//        $user->title = "นาย";
-//        $user->first_name = "admin";
-//        $user->last_name = "admin";
-//        $user->email = "admin@gmail.com";
-//        $user->password = Hash::make('1234');
-//        $user->birth_date = NOW();
-//        $user->gender = 1;
-//        $user->citizen_id = "1234567890123";
-//        $user->address = "homeless";
-//        $user->phone_number_1 = "0868214563";
-//        $user->phone_number_2 = null;
-//        $user->role = "admin";
-//        $user->checkIn_at = null;
-//        $user->save();
+        $user = new User();
+        $user->room_id = null;
+        $user->title = "นาย";
+        $user->first_name = "admin";
+        $user->last_name = "admin";
+        $user->email = "admin@gmail.com";
+        $user->password = Hash::make('1234');
+        $user->birth_date = NOW();
+        $user->gender = 1;
+        $user->citizen_id = "1234567890123";
+        $user->address = "homeless";
+        $user->phone_number_1 = "0868214563";
+        $user->phone_number_2 = null;
+        $user->role = "admin";
+        $user->checkIn_at = null;
+        $user->save();
 
         $user = new User();
         $user->room_id = null;
